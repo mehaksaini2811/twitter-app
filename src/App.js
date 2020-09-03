783d236fe3bbab6b9faeb85b328e1c21b9359da7
Mehak Saini <mehaksaini2811@gmail.com>
2020-09-03 15:54:48 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Login from './Pages/Login';
-import RegisterUserModal from './Pages/RegisterUserModal';
-import Test from './Pages/Test';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Login from "./Pages/Login";
+import RegisterUserModal from "./Pages/RegisterUserModal";
+import Test from "./Pages/Test";
 
-
+//TODO: Improve code!
 function App() {
-  return (
-    <Login/>
-  );
+  return <Login />;
 }
 
 export default App;
