a36dff52077efb8694065705444fa56cfb7878b6
Mehak Saini <mehaksaini2811@gmail.com>
2020-09-12 15:06:54 +0530

    yup integrated with hook-form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers";
+import * as yup from "yup";
 import {
   Form,
   FormGroup,
@@ -9,34 +11,35 @@ import {
   Message
 } from "rsuite";
 function LoginForm() {
-  const { register, handleSubmit, errors } = useForm();
   const onSubmit = data => console.log(data);
+  const loginSchema = yup.object().shape({
+    email: yup.string().email().required("Please enter your email"),
+    password: yup.string().required("Please enter your password")
+  });
+  const { register, handleSubmit, errors } = useForm({
+    resolver: yupResolver(loginSchema)
+  });
   return (
     <>
       <Form fluid onSubmit={handleSubmit(onSubmit)}>
         <FormGroup>
           <ControlLabel htmlFor="email">Email</ControlLabel>
-          <FormControl
-            name="email"
-            type="email"
-            inputRef={register({
-              required: true,
-              pattern: "[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2, 4}$"
-            })}
-          />
+          <FormControl name="email" inputRef={register} />
           {errors.email && (
-            <Message type="error" description="Email is required" />
+            <Message
+              type="error"
+              description={<p>{errors.email.message}</p>}
+            />
           )}
         </FormGroup>
         <FormGroup>
           <ControlLabel htmlFor="password">Password</ControlLabel>
-          <FormControl
-            type="password"
-            name="password"
-            inputRef={register({ required: true })}
-          />
+          <FormControl type="password" name="password" inputRef={register} />
           {errors.password && (
-            <Message type="error" description="Password is required" />
+            <Message
+              type="error"
+              description={<p>{errors.password.message}</p>}
+            />
           )}
         </FormGroup>
         <Button type="submit">Login</Button>
